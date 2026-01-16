02b04e5ca952d22825f101a4c2e1b7005cb3f0aa
Bades <39216599+MrBades@users.noreply.github.com>
2026-01-16 23:06:53 +0100

    Change STATIC_ROOT to use 'staticfiles_build'

--- backend/mystarlinkstats/settings.py
+++ backend/mystarlinkstats/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
